ab7b93e3e165b821155cb9d03afcbe646eb1f881
engs-alexsandrosouza-dev <engs-alexsandrosouza@camporeal.edu.br>
2026-09-08 19:43:58 -0300

    2..
    Added a function to create and manipulate a dynamic array.

--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,29 @@
-s
+#include <iostream>
+#include <memory>
+using namespace std;
+
+void vetor(){
+    int n;
+    cout << "Informe quantas posições terá o seu vetor: ";
+    cin >> n;
+    int* arr = new int [n];
+    cout << "Informe os valores de seu vetor: ";
+    
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        arr[i] = 100*i;
+    }
+    
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    
+    delete[] arr;
+    arr = nullptr;
+}
+
+int main(){
+    
+    vetor();
+    return 0;
+}
