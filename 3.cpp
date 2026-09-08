7be7bfe84be53c8a0f7eb8e8e75abac16331845c
engs-alexsandrosouza-dev <engs-alexsandrosouza@camporeal.edu.br>
2026-09-08 19:38:00 -0300

    3.3

--- 3.cpp
+++ 3.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
+#include <memory>
 using namespace std;
 
-void mostrar(){
-    cout << "Qual o tamanho do vetor? ";
+int** alocar(int n) {
+	int** vetor = new int *[n];
+	for(int i = 0; i < n; i++) {
+		vetor[i] = new int;
+	}
+
+	return vetor;
 }
-int vetor (int* arr, int tam){
-    int* v = new int[tam];
-    for (int i = 0; i < tam; i++){
-        cin >> v[i];
-        v[i] = arr[i] + 100*v[i];
+void alocarVetor(int** vetor, int n) {
+	cout << "Quais os valores do vetor:" << endl;
+	for(int i = 0; i < n; i++) {
+		cin >> *vetor[i];
+
+	}
+}
+
+void preencher(int** vetor, int n) {
+	for(int i = 0; i < n; i++) {
+		*vetor[i] = 100 * i;
+	}
+}
+
+void mostrarVetor(int** vetor, int n){
+    
+    for (int i = 0; i < n; i++){
+        cout << *vetor[i] << " ";
     }
 }
 
-void mostrarVetor(int* arr, int tam){
-    for (int i = 0; i < tam; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+
+void liberar(int** vetor, int n) {
+	for(int i = 0; i < n; i++) {
+		delete vetor[i];
+	}
+	delete[] vetor;
 }
 
-void liberarMemoria(int* arr){
-    delete[] arr;
+int main() {
+	int n;
+	cout << "Qual o tamanho do seu vetor: " << endl;
+	cin >> n;
+
+	if(n <= 0) {
+		cout <<"Valor inválido: ";
+		return 0;
+	}
+
+	int **vetor = alocar(n);
+	preencher(vetor,n);
+	alocarVetor(vetor, n);
+	mostrarVetor(vetor, n);
+	liberar(vetor, n);
 }
 
-int main(){
-    int tam;
-    mostrar();
-    cin >> tam;
-    int* arr = new int[tam];
-    vetor(arr, tam);
-    mostrarVetor(arr, tam);
-    liberarMemoria(arr);
-    return 0;
-}
+
